978b45814dde2a59da350b444c7b4ab2f41a28c9
sam <sam@Mac.lan>
2025-06-12 20:03:41 +0530

    boilerplate code with gradle

---
+++ droplite-backend/src/test/java/com/droplite/DropliteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droplite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropliteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
